a80be5065ee5d4db7f489aadf70c35dae26243a8
james-muriithi <muriithijames556@gmail.com>
2019-09-12 00:35:51 +0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,128 +1,119 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+ //////////////
+ // seat map //
+ //////////////
+ let vip_price = 1500;
+ let economy_price = 1200;
+ let firstSeatLabel = 1;
+ var details = [];
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
+ var $cart = $('#selected-seats'),
+     $counter = $('#counter'),
+     $total = $('#total'),
+     sc = $('#seat-map').seatCharts({
+         map: [
+                '___f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'ee_f',
+                'eeef',
+         ],
+         seats: {
+             f: {
+                 price: vip_price,
+                 classes: 'first-class seater_35', //your custom CSS class
+                 category: 'First Class'
+             },
+             e: {
+                 price: economy_price,
+                 classes: 'economy-class', //your custom CSS class
+                 category: 'Economy Class'
+             }
+
+         },
+         naming: {
+             top: false,
+             getLabel: function(character, row, column) {
+                 return firstSeatLabel++;
+             },
+         },
+         // legend: {
+         //     node: $('#legend'),
+         //     items: [
+         //         ['f', 'available', 'First Class'],
+         //         ['e', 'available', 'Economy Class'],
+         //         ['f', 'unavailable', 'Already Booked']
+         //     ]
+         // },
+         click: function() {
+             //alert(vip_price);
+             if (this.status() == 'available') {
+                 //let's create a new <li> which we'll add to the cart items
+                 $('<li class="p-b-4">' + this.data().category + ' Seat # ' +
+                         this.settings.label + ': <b>Ksh ' + this.data().price +
+                         '</b> <a href="javascript:void(0);"' +
+                         ' class="cancel-cart-item btn btn-danger btn-sm"><i class="fa fa-trash"></i> cancel</a></li>')
+                     .attr('id', 'cart-item-' + this.settings.id)
+                     .data('seatId', this.settings.id)
+                     .appendTo($cart);
+
+                 /*
+                  * Lets update the counter and total
+                  *
+                  * .find function will not find the current seat, because it will change its stauts only after return
+                  * 'selected'. This is why we have to add 1 to the length and the current seat price to the total.
+                  */
+                 $counter.text(sc.find('selected').length + 1);
+                 $total.text(recalculateTotal(sc) + this.data().price);
+                 details.push({
+                     ['seatNo']: this.settings.label,
+                     ['price']: this.data().price
+                 });
+
+                 return 'selected';
+             } else if (this.status() == 'selected') {
+                 //update the counter
+                 $counter.text(sc.find('selected').length - 1);
+                 //and total
+                 $total.text(recalculateTotal(sc) - this.data().price);
+
+                 //remove the item from our cart
+                 $('#cart-item-' + this.settings.id).remove();
+                 no = this.settings.label;
+                 var filtered = details.filter(function(item) {
+                     return item.seatNo != no;
+                 });
+                 details = filtered;
+
+                 //seat has been vacated
+                 return 'available';
+             } else if (this.status() == 'unavailable') {
+                 //seat has been already booked
+                 return 'unavailable';
+             } else {
+                 return this.style();
+             }
+         }
+     });
+
+let recalculateTotal = (sc) => {
+    var total = 0;
+
+    //basically find every selected seat and sum its price
+    sc.find('selected').each(function() {
+        total += this.data().price;
+    });
+
+    return total;
+}
+//this will handle "[cancel]" link clicks
+$('#selected-seats').on('click', '.cancel-cart-item', function() {
+    //let's just trigger Click event on the appropriate seat, so we don't have to repeat the logic here
+    sc.get($(this).parents('li:first').data('seatId')).click();
 });
-*/
-
-/* Otherwise just put the config content (json): */
-
-particlesJS('particles-js',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#aaabb8"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 8
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
-);
---
+++ plugins/material-datetimepicker/datetimepicker.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#date').bootstrapMaterialDatePicker({
+        time: false,
+        clearButton: true,
+        minDate: new Date()
+    });
+});
