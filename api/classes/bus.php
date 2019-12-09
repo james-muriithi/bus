0af59bf8720009ce190d958c8d721fa2fdde7b1b
james-muriithi <muriithijames556@gmail.com>
2019-12-09 09:38:57 +0300

    new changes

--- api/classes/bus.php
+++ api/classes/bus.php
@@ -1,7 +1,8 @@
 <?php
+include_once '../database/dbConnection.php';
 class Bus{
     /**
-     * @var connection
+     * @var PDO
      */
     private $conn;
     /**
@@ -10,11 +11,29 @@ class Bus{
     private $bus_id;
 
     /**
-     *@param connection $conn database connection
      * @param int $bus_id bus id
      */
-    public function __construct($conn, $bus_id){
-        $this->conn = $conn;
+    public function __construct($bus_id){
+        $database = new Database();
+        $this->conn = $database::getInstance();
+        $this->bus_id = $bus_id;
+
+        print_r($this->conn);
+    }
+
+    /**
+     * @return int
+     */
+    public function getBusId()
+    {
+        return $this->bus_id;
+    }
+
+    /**
+     * @param int $bus_id
+     */
+    public function setBusId($bus_id)
+    {
         $this->bus_id = $bus_id;
     }
 }
--- api/database/dbConnection.php
+++ api/database/dbConnection.php
@@ -4,26 +4,35 @@
  */
 class Database{
 	// database host
-	public static $conn;
+    /**
+     * @var PDO
+     */
+    public static $conn;
 
-	public static function getInstance(){
+    /**
+     * @return PDO
+     */
+    public static function getInstance(){
 		if (!isset($conn)) {
 			try {
 				$ini = parse_ini_file('app.ini');
-	        	self::$conn = new PDO("mysql:host=" . $ini['host'] . ";dbname=" . $ini['db_name'], $ini['db_user'], $ini['db_password']);
-	        	self::$conn->exec("set names utf8");
+	        	self::$conn = new PDO('mysql:host=' . $ini['host'] . ';dbname=' . $ini['db_name'], $ini['db_user'], $ini['db_password']);
+	        	self::$conn->exec('set names utf8');
 	        } catch (PDOException $e) {
-	        	echo "Connection error: " . $e->getMessage()." in file '".$e->getFile()."' on line ".$e->getLine();
+	        	echo 'Connection error: ' . $e->getMessage()." in file '".$e->getFile()."' on line ".$e->getLine();
 	        }
 		}
         return self::$conn;
 	}
-	static function clean($str){
+
+    /**
+     * @param $str string to clean
+     * @return string
+     */
+    static function clean($str){
 		$str = trim($str);
 		$str = htmlspecialchars($str);
 		$str = strip_tags($str);
 		return $str;
 	}
 }
-
-?>
