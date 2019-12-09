ec7e9bc0088f8298230f45e150bee1c52f987ca6
james-muriithi <muriithijames556@gmail.com>
2019-12-09 12:07:54 +0300

    new changes

--- api/classes/bus.php
+++ api/classes/bus.php
@@ -4,7 +4,8 @@ include_once '../database/dbConnection.php';
 /**
  * Class Bus
  */
-class Bus{
+class Bus
+{
     /**
      * @var PDO
      * @var string
@@ -18,7 +19,8 @@ class Bus{
     /**
      * @param int $bus_id bus id
      */
-    public function __construct($bus_id = 0){
+    public function __construct($bus_id = 0)
+    {
         $database = new Database();
         $this->conn = $database::getInstance();
         $this->bus_id = $bus_id;
@@ -43,14 +45,52 @@ class Bus{
     /**
      * @return mixed
      */
-    public function getBusDetails()
+    public function getBusDetails(): array
     {
-        $query = 'SELECT * FROM ' .$this->table_name. ' WHERE id= :id LIMIT 0,1';
+        $query = 'SELECT * FROM ' . $this->table_name . ' WHERE id= :id LIMIT 0,1';
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
 
-        $stmt->bindParam(':id',$this->bus_id);
+        $stmt->bindParam(':id', $this->bus_id);
+
+        $stmt->execute();
+
+        return @$stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+    }
+
+    /**
+     * @param $bus_id int optional bus id
+     * @return array
+     */
+    public function getBookedSeats($bus_id = 0): array
+    {
+        $bus_id = $bus_id == 0 ? $this->bus_id : $bus_id;
+
+        $query = 'SELECT
+                    b.id,
+                    c.fullname,
+                    c.id_number,
+                    c.phone,
+                    c.email,
+                    b.route,
+                    b.seat_no,
+                    buses.number_plate,
+                    buses.bus_name,
+                    buses.seats,
+                    b.arrival_time,
+                    b.departure_time,
+                    b.booking_date,
+                    b.paid
+                FROM
+                    bookings b
+                LEFT JOIN customers c ON b.customer_id = c.id
+                LEFT JOIN buses ON b.bus = buses.id ';
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(':id', $this->bus_id);
 
         $stmt->execute();
 
